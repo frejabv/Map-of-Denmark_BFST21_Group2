d44f75a0c371ca4bafb35bb5fdfee95ff184c4f2
frev <frev@itu.dk>
2021-03-25 18:34:30 +0100

    Temporary implementation of sorting out building and inner role to draw from relation

--- app/src/main/java/bfst21/osm/Relation.java
+++ app/src/main/java/bfst21/osm/Relation.java
@@ -1,5 +1,7 @@
 package bfst21.osm;
 
+import javafx.scene.canvas.GraphicsContext;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +18,57 @@ public class Relation extends Member {
 
     public List<Member> getMembers() {
         return members;
+    }
+
+    public void draw(GraphicsContext gc, RenderingStyle style) {
+        if(!tags.isEmpty()) {
+            gc.setStroke(style.getColorByTag(tags.get(0)));
+            gc.setFill(style.getColorByTag(tags.get(0)));
+
+            //vil tjekke om den vi skal til at tegne har tag building,
+            // i så fald kalder vi en anden tegne metode med even odd.
+
+            if(tags.contains(Tag.BUILDING)) {
+                drawBuilding(gc);
+                System.out.println("relation building drawn");
+            } else {
+                for(Member member : members) {
+                    if(member instanceof Way) {
+                        ((Way) member).draw(gc);
+                        gc.fill();
+                        System.out.println("relation drawn");
+                    }
+                }
+            }
+            /*boolean innerDrawn = false;
+            for (Member member : members) {
+                if(member instanceof Way) {
+                    member.getRoleMap().forEach((key, value) -> {
+                        if (value.equals("inner")) {
+                            ((Way) member).draw(gc);
+                            innerDrawn = true;
+                        }
+                    });
+                    ((Way) member).draw(gc);
+
+                    gc.fill();
+                }
+            }*/
+        }
+    }
+
+    public void drawBuilding(GraphicsContext gc) {
+
+        /*for(Member member : members) {
+            if(member instanceof Way) {
+                // her vil jeg tjekke om dette member har rollen inner
+                for(String value : member.getRoleMap().values()) {
+                    if(value.equals("inner")) {
+                        ((Way) member).draw(gc);
+
+                    }
+                }
+            }
+        }*/
     }
 }
