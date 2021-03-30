c73cba66d719d1dc940157e3a33c55619806201d
frev <frev@itu.dk>
2021-03-30 19:28:15 +0200

    Set isPlace correctly and comment some print statements

--- app/src/main/java/bfst21/osm/RadixTree.java
+++ app/src/main/java/bfst21/osm/RadixTree.java
@@ -3,8 +3,6 @@ package bfst21.osm;
 import java.util.ArrayList;
 
 public class RadixTree {
-    //some kind of thing to store them all in, a graph right?
-    //or whatever kd-tree does.
     private RadixNode root;
     private int size;
 
@@ -40,11 +38,12 @@ public class RadixTree {
                 return null;
             }
         }
+        System.out.println(result.equals(searchTerm) + " " + result + " " + searchTerm);
         return result;
     }
 
     public void insert(String road) {
-        System.out.println("insertion called for: " + road);
+        //System.out.println("insertion called for: " + road);
         insert(road, root);
     }
 
@@ -52,6 +51,7 @@ public class RadixTree {
         //this would be some kind of break?
         if (currentNode != root && (currentNode == null || currentNode.getContent().equals(""))) {
             System.out.println("Something is fishy?");
+            return;
         }
 
         if (currentNode.getChildren().size() == 0) {
@@ -66,45 +66,48 @@ public class RadixTree {
 
         ArrayList<RadixNode> children = currentNode.getChildren();
         for (int i = 0; i < children.size(); i++) {
-            System.out.println("currently looking at node: " + children.get(i).getContent());
+            //System.out.println("currently looking at node: " + children.get(i).getContent());
             if (road.startsWith(children.get(i).getContent())) { //the child is a prefix to road, like child: test and road: tester
                 System.out.println("recursion called for: " + road);
                 System.out.println("with new string: " + road.substring(children.get(i).getContent().length()));
                 insert(road.substring(children.get(i).getContent().length()), currentNode.getChildren().get(i));
-                System.out.println("recursion came back for: " + road);
+                //System.out.println("recursion came back for: " + road);
                 return;
             } else if (children.get(i).getContent().startsWith(road)) { //road is a prefix to child, like child: tester and road: test
-                System.out.println("current node is longer than: " + road);
                 RadixNode temp = children.get(i);
                 RadixNode node = new RadixNode(road);
+                node.setIsPlace(true);
                 children.set(i, node);
                 temp.setContent(temp.getContent().substring(road.length()));
                 children.get(i).addChild(temp);
                 size++;
-                System.out.println(temp.getContent() + " was moved down");
+                //System.out.println("current node is longer than: " + road);
+                //System.out.println(temp.getContent() + " was moved down");
                 return;
             } else if (children.get(i).getContent().charAt(0) == road.charAt(0)) { //they are partly equal like test and team
                 String nodeContent = children.get(i).getContent();
                 for (int j = 0; j < (Math.min(road.length(), nodeContent.length())); j++) {
                     if (road.charAt(j) != nodeContent.charAt(i) && j > 0) {
-                        System.out.println("more than zero chars was in common between: " + nodeContent + " " + road);
                         RadixNode temp = children.get(i);
                         temp.setContent(nodeContent.substring(j));
-                        RadixNode node = new RadixNode(road.substring(0, j));
-                        System.out.println("new parent: " + node.getContent());
-                        children.set(i, node);
-                        children.get(i).addChild(new RadixNode(road.substring(j)));
-                        children.get(i).addChild(temp);
                         RadixNode temp2 = new RadixNode(road.substring(j));
-                        System.out.println("children: " + temp.getContent() + " " + temp2.getContent());
+                        temp2.setIsPlace(true);
+                        RadixNode node = new RadixNode(road.substring(0, j));
+                        children.set(i, node);
+                        children.get(i).addChild(temp);
+                        children.get(i).addChild(temp2);
+                        /*System.out.println("more than zero chars was in common between: " + nodeContent + " " + road);
+                        System.out.println("new parent: " + node.getContent());
+                        System.out.println("children: " + temp.getContent() + " " + temp2.getContent());*/
                         return;
                     }
                 }
             } else if (i == children.size() - 1) { //none of the children contain road
-                System.out.println("there were children but " + road + " was added");
                 RadixNode newNode = new RadixNode(road);
                 currentNode.getChildren().add(newNode);
+                newNode.setIsPlace(true);
                 size++;
+                //System.out.println("there were children but " + road + " was added");
                 return;
             }
         }
