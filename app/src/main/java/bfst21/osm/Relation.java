31d2160ae58c275a988069ac39068274f1653a11
frev <frev@itu.dk>
2021-05-06 22:15:30 +0200

    Debug statements because one boi on Bornholm is broken

--- app/src/main/java/bfst21/osm/Relation.java
+++ app/src/main/java/bfst21/osm/Relation.java
@@ -41,10 +41,13 @@ public class Relation extends Member implements Drawable {
             gc.setFillRule(FillRule.EVEN_ODD);
             gc.beginPath();
 
+            if(id == 2325011 || id == 2191417) System.out.println("IT'S THE CHOSEN ONE AAAAAAAAAAAAAH !!!!!!!!!!!!!!!!!!!!!!!");
+
             for (Way way : ways) {
                 String value = way.getRoleMap().get(id);
                 if (value.equals("inner")) {
                     way.drawRelationPart(gc);
+                    System.out.println("inner drawn");
                     innerDrawn = true;
                 }
                 if (value.equals("outer")) {
@@ -52,20 +55,25 @@ public class Relation extends Member implements Drawable {
                 }
             }
 
+            System.out.println("Size of outers: " + outerLines.size());
             ArrayList<Way> mergedList;
             if (outerLines.size() > 1) {
                 mergedList = mergeOuter(outerLines);
+                System.out.println("Size of merged: " + mergedList.size());
+
             } else {
                 mergedList = outerLines;
             }
 
             if (innerDrawn) {
                 for (Way way : mergedList) {
+                    System.out.println("outer drawn");
                     way.drawRelationPart(gc);
                 }
                 gc.fill();
             } else {
                 //draw relation normally
+                System.out.println("something normal");
                 gc.setFillRule(FillRule.NON_ZERO);
                 for (Way way : ways) {
                     var drawStyle = renderingStyle.getDrawStyleByTag(tags.get(0));
@@ -75,19 +83,31 @@ public class Relation extends Member implements Drawable {
                     }
                 }
             }
+            System.out.println();
         }
     }
 
     public ArrayList<Way> mergeOuter(ArrayList<Way> outerLines) {
         HashMap<Node, Way> pieces = new HashMap<>();
-        for (var line : outerLines) {
-            var before = pieces.remove(line.first());
-            var after = pieces.remove(line.last());
+        for (Way line : outerLines) {
+            System.out.println(line.getId());
+            System.out.println("First and last nodes: " + line.first().getId() + " " + line.last().getId());
+            Way before = pieces.remove(line.first());
+            Way after = pieces.remove(line.last());
             if (before == after)
                 after = null;
             var merged = Way.merge(before, line, after);
-            pieces.put(merged.first(), merged);
-            pieces.put(merged.last(), merged);
+            var merged1 = Way.merge(before, merged);
+            var merged2 = Way.merge(merged1, after);
+            if(before != null) {
+                System.out.println("First of before: " + before.first().getId());
+            }
+            if(after != null) {
+                System.out.println( "Last of after: " + after.last().getId());
+            }
+            System.out.println("New first and last nodes: " + merged2.first().getId() + " " + merged2.last().getId());
+            pieces.put(merged2.first(), merged2);
+            pieces.put(merged2.last(), merged2);
         }
         ArrayList<Way> mergedList = new ArrayList<>();
         pieces.forEach((node, way) -> {
