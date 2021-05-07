64d5ae5271930fdb182c0b4f20a8b74ccefd59fc
frev <frev@itu.dk>
2021-05-07 11:11:43 +0200

    Fix merge relations that get read in the wrong direction

--- app/src/main/java/bfst21/osm/Relation.java
+++ app/src/main/java/bfst21/osm/Relation.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.FillRule;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -41,13 +42,10 @@ public class Relation extends Member implements Drawable {
             gc.setFillRule(FillRule.EVEN_ODD);
             gc.beginPath();
 
-            if(id == 2325011 || id == 2191417) System.out.println("IT'S THE CHOSEN ONE AAAAAAAAAAAAAH !!!!!!!!!!!!!!!!!!!!!!!");
-
             for (Way way : ways) {
                 String value = way.getRoleMap().get(id);
                 if (value.equals("inner")) {
                     way.drawRelationPart(gc);
-                    System.out.println("inner drawn");
                     innerDrawn = true;
                 }
                 if (value.equals("outer")) {
@@ -55,11 +53,9 @@ public class Relation extends Member implements Drawable {
                 }
             }
 
-            System.out.println("Size of outers: " + outerLines.size());
             ArrayList<Way> mergedList;
             if (outerLines.size() > 1) {
                 mergedList = mergeOuter(outerLines);
-                System.out.println("Size of merged: " + mergedList.size());
 
             } else {
                 mergedList = outerLines;
@@ -67,13 +63,11 @@ public class Relation extends Member implements Drawable {
 
             if (innerDrawn) {
                 for (Way way : mergedList) {
-                    System.out.println("outer drawn");
                     way.drawRelationPart(gc);
                 }
                 gc.fill();
             } else {
                 //draw relation normally
-                System.out.println("something normal");
                 gc.setFillRule(FillRule.NON_ZERO);
                 for (Way way : ways) {
                     var drawStyle = renderingStyle.getDrawStyleByTag(tags.get(0));
@@ -83,31 +77,22 @@ public class Relation extends Member implements Drawable {
                     }
                 }
             }
-            System.out.println();
         }
     }
 
     public ArrayList<Way> mergeOuter(ArrayList<Way> outerLines) {
         HashMap<Node, Way> pieces = new HashMap<>();
         for (Way line : outerLines) {
-            System.out.println(line.getId());
-            System.out.println("First and last nodes: " + line.first().getId() + " " + line.last().getId());
             Way before = pieces.remove(line.first());
             Way after = pieces.remove(line.last());
+            if(after != null && line.last() != after.first()) {
+                Collections.reverse(after.getNodes());
+            }
             if (before == after)
                 after = null;
             var merged = Way.merge(before, line, after);
-            var merged1 = Way.merge(before, merged);
-            var merged2 = Way.merge(merged1, after);
-            if(before != null) {
-                System.out.println("First of before: " + before.first().getId());
-            }
-            if(after != null) {
-                System.out.println( "Last of after: " + after.last().getId());
-            }
-            System.out.println("New first and last nodes: " + merged2.first().getId() + " " + merged2.last().getId());
-            pieces.put(merged2.first(), merged2);
-            pieces.put(merged2.last(), merged2);
+            pieces.put(merged.first(), merged);
+            pieces.put(merged.last(), merged);
         }
         ArrayList<Way> mergedList = new ArrayList<>();
         pieces.forEach((node, way) -> {
