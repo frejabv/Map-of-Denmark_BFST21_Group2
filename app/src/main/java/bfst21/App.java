0e2e4d1d422deb050e455b2011b71003f66a411b
Ib Løwe Skovfoged Scherer <ibsc@itu.dk>
2021-05-09 17:40:33 +0200

    changed file to be opened from "map" to "bornholm"

--- app/src/main/java/bfst21/App.java
+++ app/src/main/java/bfst21/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
         if (fileSelectorMode) {
             new StartupScreen(primaryStage);
         } else {
-            var model = new Model("/bfst21/data/map.zip", ttiMode);
+            var model = new Model("/bfst21/data/bornholm.osm", ttiMode);
             new View(model, primaryStage);
         }
     }
