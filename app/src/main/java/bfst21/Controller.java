36902277a0cd7087917fa9b0b9fd8948d88d6def
jeppekorgaard <korg@itu.dk>
2021-05-17 19:53:21 +0200

    replace , with .

--- app/src/main/java/bfst21/Controller.java
+++ app/src/main/java/bfst21/Controller.java
@@ -354,8 +354,8 @@ public class Controller {
             removePinContainer.getChildren().removeAll(removePinContainer.lookup(".button"));
             Point2D coordinates = canvas.setPin(new Point2D(e.getX(), e.getY()));
             DecimalFormat df = new DecimalFormat("#.#####");
-            String xCoordinate = df.format(coordinates.getX());
-            String yCoordinate = df.format(coordinates.getY() * -model.scalingConstant);
+            String xCoordinate = df.format(coordinates.getX()).replace(",",".");
+            String yCoordinate = df.format(coordinates.getY() * -model.scalingConstant).replace(",",".");
             if (currentPOI != null && currentPOI.getX() != canvas.getPinPoint().getX() || currentPOI != null && currentPOI.getY() != canvas.getPinPoint().getY()) {
                 currentPOI = null;
                 heartIcon.setImage(new Image(getClass().getResource("/bfst21/icons/heart-border.png").toString()));
