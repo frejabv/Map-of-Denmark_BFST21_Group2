c6a0c0af827244afa94fa7bb1112a7ec0a086fcb
Jacob Møller <jmj1999@gmail.com>
2021-05-17 11:22:21 +0200

    Loading test

--- app/src/main/java/bfst21/Startup/StartupController.java
+++ app/src/main/java/bfst21/Startup/StartupController.java
@@ -5,6 +5,9 @@ import bfst21.Model;
 import bfst21.View;
 import bfst21.osm.FileExtension;
 import bfst21.osm.OSMParser;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -12,12 +15,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
 
 public class StartupController {
     Stage stage;
 
+    @FXML
+    private Text loadingText;
+
     public void init(Stage stage) {
         this.stage = stage;
+        loadingText.setText("something");
     }
 
     FileChooser fileChooser = new FileChooser();
@@ -31,6 +40,26 @@ public class StartupController {
     }
 
     public void defaultFile() {
+        setStyling();
+        Task <Void> task = new Task<Void>() {
+            @Override public Void call() throws InterruptedException {
+                setStyling();
+                return null;
+            }
+        };
+        task.setOnSucceeded(e -> {
+            openfile();
+        });
+
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+
+
+
+    public void openfile() {
         try {
             var model = new Model("/bfst21/data/bornholm.osm", false);
             new View(model, stage);
@@ -55,5 +84,15 @@ public class StartupController {
 
     public void exit() {
         System.exit(0);
+    }
+
+    public void setStyling() {
+        System.out.println("set styling was called");
+        loadingText.setText("something eldr");
+        //loadingText.setVisible(true);
+        //loadingText.setManaged(true);
+        //loadingText.setStyle("visibility: hidden");
+        //loadingText.setStyle("-fx-opacity: .5");
+        //openButton.setStyle("-fx-opacity: .5");
     }
 }
